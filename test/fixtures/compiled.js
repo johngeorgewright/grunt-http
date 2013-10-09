b61f38dd8f4604bf65a8efa10d3251284cbdb59c
Andy Royle <ajroyle@gmail.com>
2013-10-09 16:29:13 +0100

    make closure compilation test pass

--- test/fixtures/compiled.js
+++ test/fixtures/compiled.js
@@ -1,3 +1,3 @@
 var request=require("request");
-module.exports=function(b){b.registerMultiTask("http","Sends a HTTP request and deals with the response.",function(){var a=this.data,e=this.async(),d=!1;a.dest&&(d=a.dest,delete a.dest);a.url||b.fail.fatal("The http task requires a URL");b.log.subhead("Request");b.log.writeln(JSON.stringify(a,null,2));var f=d;request(a,function(a,c,d){b.log.subhead("Response");if(a)return b.fail.fatal(a),e(a);if(200>c.statusCode||299<c.statusCode)return b.fail.fatal(c.statusCode),e(c.statusCode);b.log.ok(c.statusCode);
-f&&b.file.write(f,d);e()})})};
+module.exports=function(b){function f(a,e,d){return function(g,c,f){c=c||{statusCode:0};b.log.subhead("Response");if(g&&!d)return b.fail.fatal(g),a(g);if(!d&&(200>c.statusCode||299<c.statusCode))return b.fail.fatal(c.statusCode),a(c.statusCode);b.log.ok(c.statusCode);e&&b.file.write(e,f);a()}}b.registerMultiTask("http","Sends a HTTP request and deals with the response.",function(){var a=this.data,e=this.async(),d=!1;a.dest&&(d=a.dest,delete a.dest);a.url||b.fail.fatal("The http task requires a URL");b.log.subhead("Request");
+b.log.writeln(JSON.stringify(a,null,2));request(a,f(e,d,a.ignoreErrors))})};
